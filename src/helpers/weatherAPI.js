63349059972b7ffb17300b9404ac5fa467c00d4f
Alex Vido <alex.vido93@gmail.com>
2023-05-25 14:57:56 -0300

    Projeto iniciado 🚀

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,9 @@
 // Remova os comentários a medida que for implementando as funções
 
-export const searchCities = (/* term */) => {
+export const searchCities = (term) => {
 //   seu código aqui
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
+export const getWeatherByCity = (cityURL) => {
 //   seu código aqui
 };
